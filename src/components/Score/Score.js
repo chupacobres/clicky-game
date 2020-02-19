9459d1c70644d481f62395558bb3bf3cec9d2636
chupacobres <50712370+chupacobres@users.noreply.github.com>
2020-02-19 17:14:12 -0500

    CSS fixes

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -5,7 +5,7 @@ import "./Score.css";
 //stateless component
 const Score = props => (
   <div className="gameScore">
-    <h3 className="score">Your Score {props.total}</h3>
+    <h3 className="score">Score {props.total}</h3>
     <h3 className="status">{props.status}</h3>
   </div>
 );
